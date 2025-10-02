c818040a8985e3441cddaeaf361ce35eae7e9fed
Cryptogeek01 <simonprosper500@gmail.com>
2025-10-02 21:33:22 +0100

    Update VestingCliffTrap.sol

--- VestingCliffTrap.sol
+++ VestingCliffTrap.sol
@@ -27,6 +27,14 @@ contract VestingCliffTrap is ITrap {
     /// Decide if response is needed
     function shouldRespond(bytes[] calldata data) external pure override returns (bool, bytes memory) {
         (bool cliffPassed, address vesting, uint256 unlockTime) = abi.decode(data[0], (bool, address, uint256));
+        if (data.length == 0) {
+           return (false, address(0),"");
+        }
+
+     //Decode data[0] safely
+        (bool cliffpassed, address vesting, uint256 unlockTime) = abi.decode(data[0], (bool, address, uint256));
+        (, , address responseContract) = IVestingCliffConfig(configContract).getConfig(address(this));
+
         if (cliffPassed) {
             return (true, abi.encode(vesting, unlockTime));
         }
